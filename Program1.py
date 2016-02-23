fb2f224b561a3ebc39b1e7f7819d6828d0490097
Alexander Parsan <alexanderparsan28@gmail.com>
2016-02-23 17:40:31 -0500

    Create Program1.py

---
+++ Program1.py
@@ -0,0 +1,21 @@
+#Program1.py 
+#Written By: Ryaan A. on Feb 23, 2016
+#Program Uploaded and Commented by: Freddy Baker and Alexander Parsan
+#Under the GPL v3.0 License
+
+print "Welcome to Fun Time"
+print "Question 1"
+print "Type 9 + 10"
+x = input()
+if x == 19:
+	print "YAY YO GOT IT"
+	print "and another one"
+	print "Whats 200+10"
+	y = input()
+	if y == 210:
+		print "YOU DID IT"
+		print "THATS ALL FOLKS"
+	elif y != 210:
+		print "WRONG"
+elif x != 19:
+	print "You Failed sorry"
